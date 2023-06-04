b3b2462345a8572ae8fa1c34134c80092e694cb4
NguyenChiThanh <101045303+ncthanh2001@users.noreply.github.com>
2023-06-04 09:49:41 +0700

    Login_Logout_Register

--- src/main/java/com/shop/Config/MvcConfig.java
+++ src/main/java/com/shop/Config/MvcConfig.java
@@ -1,14 +1,2 @@
-//package com.shop.Config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//@Configuration
-//public class MvcConfig implements WebMvcConfigurer {
-//    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/home").setViewName("index");
-//        registry.addViewController("/").setViewName("index");
-//        registry.addViewController("/hello").setViewName("login");
-//        registry.addViewController("/login").setViewName("login");
-//    }
-//}
+package com.shop.Config;public class MvcConfig {
+}
--- src/main/java/com/shop/Service/Impl/AccoutServiceImpl.java
+++ src/main/java/com/shop/Service/Impl/AccoutServiceImpl.java
@@ -2,17 +2,21 @@ package com.shop.Service.Impl;
 
 
 import com.shop.Entity.Accounts;
+import com.shop.Entity.CustomUserDetails;
 import com.shop.Repository.AccountsRepository;
 import com.shop.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @Primary
-public class AccoutServiceImpl implements AccountService {
+public class AccoutServiceImpl implements AccountService , UserDetailsService {
     @Autowired
     AccountsRepository accountsRepository;
 
@@ -27,4 +31,12 @@ public class AccoutServiceImpl implements AccountService {
     }
 
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Accounts user = accountsRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new CustomUserDetails(user);
+    }
 }
